546f96d7b17f6dcf878044795000ebfc02a5fcca
ltrwfs <ivanpanasiuk15@gmail.com>
2024-10-03 13:53:43 +0300

    Task 3 Функция с осями

--- task3.py
+++ task3.py
@@ -1,9 +1,11 @@
 def graph(length):
     width = length//2
+    print('↑')
     for lines in range(length):
         position = lines//2
-        print(' '*(width - position) + '\x1b[43m \x1b[0m' + ' '*position)
+        print('|'+' '*(width - position - 1) + '\x1b[43m \x1b[0m' + ' '*position)
+    print('0' + '—'*width + '>')
 
 if __name__ == '__main__':
-    length = 16
+    length = 14
     graph(length)
